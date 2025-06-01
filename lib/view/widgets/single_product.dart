498289f6badac1f3f97f6980f3bfa9c72214ee4a
App-Developer <rafiullahkhaleel@gmail.com>
2025-05-31 18:19:47 -0700

    commit

--- lib/view/widgets/single_product.dart
+++ lib/view/widgets/single_product.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
 class SingleProduct extends StatelessWidget {
-  const SingleProduct({super.key});
+  final String imageURL;
+  final String name;
+  final VoidCallback? onTap;
+
+  const SingleProduct({
+    super.key,
+    required this.imageURL,
+    required this.name,
+    this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -11,22 +20,21 @@ class SingleProduct extends StatelessWidget {
         margin: EdgeInsets.symmetric(horizontal: 5),
         height: 250,
         width: 200,
-        decoration: BoxDecoration(
-          color: Colors.white,
-        ),
+        decoration: BoxDecoration(color: Colors.white),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           spacing: 5,
           children: [
             Expanded(
               flex: 3,
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(
-                      'https://greengardens.pk/wp-content/uploads/2023/07/1000015570.jpg',
+              child: InkWell(
+                onTap: onTap,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(imageURL),
+                      fit: BoxFit.cover,
                     ),
-                    fit: BoxFit.cover,
                   ),
                 ),
               ),
@@ -39,12 +47,18 @@ class SingleProduct extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      'Fresh Basil',
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                      name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
                     ),
                     Text(
                       '50\$/50 Gram',
-                      style: TextStyle(fontSize: 15, color: Colors.grey.shade600),
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey.shade600,
+                      ),
                     ),
                     Expanded(
                       child: Padding(
@@ -57,14 +71,17 @@ class SingleProduct extends StatelessWidget {
                             Expanded(
                               child: Container(
                                 decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.grey.shade600),
+                                  border: Border.all(
+                                    color: Colors.grey.shade600,
+                                  ),
                                   borderRadius: BorderRadius.circular(50),
                                 ),
                                 child: Column(
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
                                     Row(
-                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
                                       children: [
                                         Text(
                                           '50 Gram',
@@ -84,7 +101,9 @@ class SingleProduct extends StatelessWidget {
                             Expanded(
                               child: Container(
                                 decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.grey.shade600),
+                                  border: Border.all(
+                                    color: Colors.grey.shade600,
+                                  ),
                                   borderRadius: BorderRadius.circular(50),
                                 ),
                                 child: Column(
